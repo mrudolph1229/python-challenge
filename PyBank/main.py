465e3e4331c17ffb81d7e7d4157fd6261f354a47
Mark Rudolph <44739691+mrudolph1229@users.noreply.github.com>
2018-12-11 22:19:45 -0700

    1211 1019 Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -32,10 +32,10 @@ with open(csvpath,'r', newline="") as csvfile:
     print ("Greatest Decrease in Profits: $" + str(min_change) +", in " + min_month)
 
     with open("Output.txt", "w") as text_file:
-        text_file.write("FINANCIAL ANALYSIS\n")
-        text_file.write("-----------------------------\n")
-        text_file.write(f"Total Months: {months}\n")
-        text_file.write(f"Total: ${net_amount}\n")
-        text_file.write(f"Average Change: ${avg_change}\n")
-        text_file.write(f"Greatest Increase in Profits: ${max_change}, in {max_month}\n")
-        text_file.write(f"Greatest Decrease in Profits: ${min_change}, in {min_month}\n")
+        text_file.write("FINANCIAL ANALYSIS\n"\
+    "-----------------------------\n"\
+    f"Total Months: {months}\n"\
+    f"Total: ${net_amount}\n"\
+    f"Average Change: ${avg_change}\n"\
+    f"Greatest Increase in Profits: ${max_change}, in {max_month}\n"\
+    f"Greatest Decrease in Profits: ${min_change}, in {min_month}")
